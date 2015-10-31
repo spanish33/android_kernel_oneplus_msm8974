fd397ed3034cab336137b424b4b99d9f29290a8c
DerRomtester <dieb.stefan.96@gmail.com>
2015-10-31 16:59:26 +0100

    cpuquiet: disabled by default
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>

--- drivers/cpuquiet/cpuquiet_driver.c
+++ drivers/cpuquiet/cpuquiet_driver.c
@@ -31,7 +31,7 @@ static struct work_struct minmax_work;
 static struct work_struct cpu_core_state_work;
 static struct kobject *auto_sysfs_kobject;
 
-static bool enabled = false;
+static bool enabled = true;
 static bool manual_hotplug = false;
 // core 0 is always active
 unsigned int cpu_core_state[3] = {0, 0, 0};
