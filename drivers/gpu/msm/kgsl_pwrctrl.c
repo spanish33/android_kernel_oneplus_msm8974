c7690b1ca427f38f496b859dc3166ce41b15ca40
Ajay Dudani <adudani@codeaurora.org>
2015-12-10 12:19:16 -0800

    msm: kgsl: Fix direct references to HZ
    Make the various timeout values HZ agnostic by using the proper
    macros and values instead.
    
    Change-Id: I906b948657c8873518042c7465272c98c5391e59
    Signed-off-by: Suman Tatiraju <sumant@codeaurora.org>
    Signed-off-by: Ajay Dudani <adudani@codeaurora.org>
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>
    
    Conflicts:
    	arch/arm/boot/dts/msm8974-gpu.dtsi
    	drivers/gpu/msm/kgsl_pwrctrl.c

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -463,6 +463,7 @@ static int kgsl_pwrctrl_idle_timer_store(struct device *dev,
 {
 	unsigned int val = 0;
 	struct kgsl_device *device = kgsl_device_from_dev(dev);
+	struct kgsl_pwrctrl *pwr;
 	const long div = 1000/msecs_to_jiffies(1000);
 	int ret;
 
