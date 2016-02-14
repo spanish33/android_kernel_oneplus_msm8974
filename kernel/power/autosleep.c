2496f1d1984b73905060b4de576da6e4c75e0087
acuicultor <ramoncuenca4@gmail.com>
2016-02-14 17:07:44 +0100

    fix derp

--- kernel/power/autosleep.c
+++ kernel/power/autosleep.c
@@ -104,10 +104,6 @@ int pm_autosleep_set_state(suspend_state_t state)
 	if (state > PM_SUSPEND_ON) {
 		pm_wakep_autosleep_enabled(true);
 		queue_up_suspend_work();
-#ifdef CONFIG_POWERSUSPEND
-		/* Yank555.lu : hook to handle powersuspend tasks (sleep) */
-		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_ACTIVE);
-#endif
 
 	} else {
 		pm_wakep_autosleep_enabled(false);
