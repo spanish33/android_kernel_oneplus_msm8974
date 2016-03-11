29b272e7190a2ddc9231810156c4b7f28ea2bddd
acuicultor <ramoncuenca4@gmail.com>
2016-03-11 15:44:13 +0100

    sched: window-based load stats for tasks
    Provide a metric per task that specifies how cpu bound a task is. Task
    execution is monitored over several time windows and the fraction of
    the window for which task was found to be executing or wanting to run
    is recorded as task's demand. Windows over which task was sleeping are
    ignored. We track last 5 recent windows for every task and the maximum
    demand seen in any of the previous 5 windows (where task had some
    activity) drives freq demand for every task.
    
    A per-cpu metric (rq->cumulative_runnable_avg) is also provided which
    is an aggregation of cpu demand of all tasks currently enqueued on it.
    rq->cumulative_runnable_avg will be useful to know if cpu frequency
    will need to be changed to match task demand.
    
    Change-Id: Ib83207b9ba8683cd3304ee8a2290695c34f08fe2
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    
    Conflicts:
    	include/linux/sched.h
    
    Conflicts:
    	include/linux/sched.h (reverted from commit d63e7101164fcdc96feeb2b447387b06337e2f12)

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1233,39 +1233,8 @@ struct sched_statistics {
 };
 #endif
 
-#define RAVG_HIST_SIZE  5
-
-/* ravg represents frequency scaled cpu-demand of tasks */
-struct ravg {
-	/*
-	 * 'window_start' marks the beginning of new window
-	 *
-	 * 'mark_start' marks the beginning of an event (task waking up, task
-	 * starting to execute, task being preempted) within a window
-	 *
-	 * 'sum' represents how runnable a task has been within current
-	 * window. It incorporates both running time and wait time and is
-	 * frequency scaled.
-	 *
-	 * 'sum_history' keeps track of history of 'sum' seen over previous
-	 * RAVG_HIST_SIZE windows. Windows where task was entirely sleeping are
-	 * ignored.
-	 *
-	 * 'demand' represents maximum sum seen over previous RAVG_HIST_SIZE
-	 * windows. 'demand' could drive frequency demand for tasks.
-	 */
-	u64 window_start, mark_start;
-	u32 sum, demand;
-	u32 sum_history[RAVG_HIST_SIZE];
-};
-
 struct sched_entity {
 	struct load_weight	load;		/* for load-balancing */
-	/*
-	 * Todo : Move ravg to 'struct task_struct', as this is common for both
-	 * real-time and non-realtime tasks
-	 */
-	struct ravg		ravg;
 	struct rb_node		run_node;
 	struct list_head	group_node;
 	unsigned int		on_rq;
@@ -2168,15 +2137,6 @@ extern unsigned int sysctl_sched_min_granularity;
 extern unsigned int sysctl_sched_wakeup_granularity;
 extern unsigned int sysctl_sched_child_runs_first;
 extern unsigned int sysctl_sched_wake_to_idle;
-extern unsigned int sysctl_sched_ravg_window;
-extern unsigned int sysctl_sched_wakeup_load_threshold;
-#ifdef CONFIG_SCHED_DEBUG
-extern __read_mostly unsigned int sysctl_sched_yield_sleep_duration;
-extern __read_mostly int sysctl_sched_yield_sleep_threshold;
-#else
-extern const unsigned int sysctl_sched_yield_sleep_duration;
-extern const int sysctl_sched_yield_sleep_threshold;
-#endif
 
 enum sched_tunable_scaling {
 	SCHED_TUNABLESCALING_NONE,
