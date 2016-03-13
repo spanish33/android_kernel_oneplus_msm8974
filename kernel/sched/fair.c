5b1a7501967bb91a020de2439dffb3b727d8b72d
acuicultor <ramoncuenca4@gmail.com>
2016-03-13 17:13:29 +0100

    Revert "TESTING!!! Revert "tracing/sched: add load balancer tracepoint""
    This reverts commit 98b483f9cf5f3899fd835e994f146c30d564cf3f.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4453,7 +4453,8 @@ static int load_balance(int this_cpu, struct rq *this_rq,
 {
 	int ld_moved, active_balance = 0;
 	struct sched_group *group;
-	struct rq *busiest;
+	unsigned long imbalance;
+	struct rq *busiest = NULL;
 	unsigned long flags;
 	struct cpumask *cpus = __get_cpu_var(load_balance_tmpmask);
 
@@ -4622,6 +4623,10 @@ out_one_pinned:
 
 	ld_moved = 0;
 out:
+	trace_sched_load_balance(this_cpu, idle, *balance,
+				 group ? group->cpumask[0] : 0,
+				 busiest ? busiest->nr_running : 0, imbalance,
+				 env.flags, ld_moved, sd->balance_interval);
 	return ld_moved;
 }
 
