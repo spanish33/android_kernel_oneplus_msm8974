684914922b9e3b40c94b58cf36cfec4ec3526710
acuicultor <ramoncuenca4@gmail.com>
2016-06-27 18:19:34 +0200

    Update build version and use sabermod gcc7 toolchain

--- radioactive-bacon-build.sh
+++ radioactive-bacon-build.sh
@@ -16,12 +16,12 @@ DEFCONFIG="radioactive_defconfig"
 
 # Kernel Details
 BASE_RADIOACTIVE_VER="RADIOACTIVE_REBORN"
-VER="_V2.46"
+VER="_V2.48"
 RADIOACTIVE_VER="$BASE_RADIOACTIVE_VER$VER"
 
 # Vars
 export LOCALVERSION=~`echo $RADIOACTIVE_VER`
-export CROSS_COMPILE=${HOME}/Android/toolchains/linaro4.9/bin/arm-eabi-
+export CROSS_COMPILE=${HOME}/Android/toolchains/sm7/bin/arm-eabi-
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=R.Cuenca
